65cd1855a5b6050af3758c1ed74ab1d7f1a7549a
Salvatory Sylves <ssylves@CRDBBANKPLC.COM>
2021-08-13 17:00:24 +0300

    my first commit

--- usr/share/perl5/vendor_perl/Date/Language/Danish.pm
+++ usr/share/perl5/vendor_perl/Date/Language/Danish.pm
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ##
 ## Danish tables
 ##
@@ -34,3 +35,41 @@ sub format_h { $MoYs[$_[0]->[4]] }
 sub format_p { $_[0]->[2] >= 12 ?  $AMPM[1] : $AMPM[0] }
 
 1;
+=======
+##
+## Danish tables
+##
+
+package Date::Language::Danish;
+
+use Date::Language ();
+use vars qw(@ISA @DoW @DoWs @MoY @MoYs @AMPM @Dsuf %MoY %DoW $VERSION);
+@ISA = qw(Date::Language);
+$VERSION = "1.01";
+
+@MoY  = qw(Januar Februar Marts April Maj Juni
+	   Juli August September Oktober November December);
+@MoYs = qw(Jan Feb Mar Apr Maj Jun Jul Aug Sep Okt Nov Dec);
+@DoW  = qw(Søndag Mandag Tirsdag Onsdag Torsdag Fredag Lørdag Søndag);
+@DoWs = qw(Søn Man Tir Ons Tor Fre Lør Søn);
+
+use Date::Language::English ();
+@AMPM =   @{Date::Language::English::AMPM};
+@Dsuf =   @{Date::Language::English::Dsuf};
+
+@MoY{@MoY}  = (0 .. scalar(@MoY));
+@MoY{@MoYs} = (0 .. scalar(@MoYs));
+@DoW{@DoW}  = (0 .. scalar(@DoW));
+@DoW{@DoWs} = (0 .. scalar(@DoWs));
+
+# Formatting routines
+
+sub format_a { $DoWs[$_[0]->[6]] }
+sub format_A { $DoW[$_[0]->[6]] }
+sub format_b { $MoYs[$_[0]->[4]] }
+sub format_B { $MoY[$_[0]->[4]] }
+sub format_h { $MoYs[$_[0]->[4]] }
+sub format_p { $_[0]->[2] >= 12 ?  $AMPM[1] : $AMPM[0] }
+
+1;
+>>>>>>> 9bc6dff (my first commit)
